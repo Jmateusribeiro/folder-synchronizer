5d19f5f5bb864837b58d0d29dd3be06d2d9cc977
Jmateusribeiro <mateusribeirojorge@gmail.com>
2024-06-02 15:32:17 +0100

    refactoring unit tests

--- tests/test_folder_operations.py
+++ tests/test_folder_operations.py
@@ -7,14 +7,14 @@ import shutil
 from unittest.mock import Mock
 import pytest
 from _pytest.fixtures import FixtureRequest
+from folder_synchronizer.classes.folder_operations import FolderSynchronizer
 
 # Adding the folder containing the folder_operations module to the path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from folder_synchronizer.classes.folder_operations import FolderSynchronizer
 
 # Global variables for the source and replica folders used in tests
-source_folder = 'test_source_folder'
-replica_folder = 'test_replica_folder'
+SOURCE_FOLDER = 'test_source_folder'
+REPLICA_FOLDER = 'test_replica_folder'
 log = Mock()
 
 @pytest.fixture(scope="module", autouse=True)
@@ -26,16 +26,16 @@ def create_folders(request: FixtureRequest) -> None:
         request (FixtureRequest): The fixture request object.
     """
     # Setup
-    os.makedirs(source_folder, exist_ok=True)
-    os.makedirs(replica_folder, exist_ok=True)
+    os.makedirs(SOURCE_FOLDER, exist_ok=True)
+    os.makedirs(REPLICA_FOLDER, exist_ok=True)
 
     def teardown() -> None:
         # Teardown
-        shutil.rmtree(source_folder)
-        shutil.rmtree(replica_folder)
+        shutil.rmtree(SOURCE_FOLDER)
+        shutil.rmtree(REPLICA_FOLDER)
 
-        assert not os.path.exists(source_folder)
-        assert not os.path.exists(replica_folder)
+        assert not os.path.exists(SOURCE_FOLDER)
+        assert not os.path.exists(REPLICA_FOLDER)
 
     # Register the teardown function to be called after tests
     request.addfinalizer(teardown)
@@ -48,13 +48,13 @@ def folder_sync() -> FolderSynchronizer:
     Returns:
         FolderSynchronizer: An instance of FolderSynchronizer.
     """
-    return FolderSynchronizer(source_folder, replica_folder, log)
+    return FolderSynchronizer(SOURCE_FOLDER, REPLICA_FOLDER, log)
 
 def test_synchronize_nonexistent_source_folder() -> None:
     """
     Test synchronizing a non-existent source folder raises FileNotFoundError.
     """
-    folder_sync_test = FolderSynchronizer('nonexistent_source', replica_folder, log)
+    folder_sync_test = FolderSynchronizer('nonexistent_source', REPLICA_FOLDER, log)
 
     with pytest.raises(FileNotFoundError):
         folder_sync_test.synchronize()
@@ -64,31 +64,31 @@ def test_synchronize_create_replica_folder() -> None:
     Test that the replica folder is created if it does not exist.
     """
     custom_replica_folder = 'custom_replica_folder'
-    folder_sync_test = FolderSynchronizer(source_folder, custom_replica_folder, log)
+    folder_sync_test = FolderSynchronizer(SOURCE_FOLDER, custom_replica_folder, log)
 
     folder_sync_test.synchronize()
 
     assert os.path.exists(custom_replica_folder)
-    
+
     shutil.rmtree(custom_replica_folder)
     assert not os.path.exists(custom_replica_folder)
 
-def test_synchronize_remove_folder(folder_sync: FolderSynchronizer) -> None:
+def test_synchronize_remove_folder(folder_sync: 'FolderSynchronizer') -> None:
     """
     Test that a folder in the replica that does not exist in the source is removed.
     """
-    empty_folder = os.path.join(replica_folder, 'folder_removal/folder')
+    empty_folder = os.path.join(REPLICA_FOLDER, 'folder_removal/folder')
     os.makedirs(empty_folder)
 
     folder_sync.synchronize()
 
     assert not os.path.exists(empty_folder)
 
-def test_synchronize_remove_file(folder_sync: FolderSynchronizer) -> None:
+def test_synchronize_remove_file(folder_sync: 'FolderSynchronizer') -> None:
     """
     Test that a file in the replica that does not exist in the source is removed.
     """
-    empty_folder = os.path.join(replica_folder, 'file_removal/folder/')
+    empty_folder = os.path.join(REPLICA_FOLDER, 'file_removal/folder/')
     os.makedirs(empty_folder)
     empty_file = os.path.join(empty_folder, 'file.txt')
     with open(empty_file, 'w', encoding='utf-8'):
@@ -98,44 +98,44 @@ def test_synchronize_remove_file(folder_sync: FolderSynchronizer) -> None:
 
     assert not os.path.exists(empty_file)
 
-def test_synchronize_create_folder(folder_sync: FolderSynchronizer) -> None:
+def test_synchronize_create_folder(folder_sync: 'FolderSynchronizer') -> None:
     """
     Test that a folder in the source is copied to the replica.
     """
     folder_name = 'folder_creation/folder/folder'
-    os.makedirs(os.path.join(source_folder, folder_name))
+    os.makedirs(os.path.join(SOURCE_FOLDER, folder_name))
 
     folder_sync.synchronize()
 
-    assert os.path.exists(os.path.join(replica_folder, folder_name))
+    assert os.path.exists(os.path.join(REPLICA_FOLDER, folder_name))
 
-def test_synchronize_create_file(folder_sync: FolderSynchronizer) -> None:
+def test_synchronize_create_file(folder_sync: 'FolderSynchronizer') -> None:
     """
     Test that a file in the source is copied to the replica.
     """
     folder_name = 'file_creation/folder/folder/'
-    os.makedirs(os.path.join(source_folder, folder_name))
+    os.makedirs(os.path.join(SOURCE_FOLDER, folder_name))
     file_name = folder_name + 'file.txt'
-    with open(os.path.join(source_folder, file_name), 'w', encoding='utf-8'):
+    with open(os.path.join(SOURCE_FOLDER, file_name), 'w', encoding='utf-8'):
         pass
 
     folder_sync.synchronize()
 
-    assert os.path.exists(os.path.join(replica_folder, file_name))
+    assert os.path.exists(os.path.join(REPLICA_FOLDER, file_name))
 
-def test_synchronize_update_file(folder_sync: FolderSynchronizer) -> None:
+def test_synchronize_update_file(folder_sync: 'FolderSynchronizer') -> None:
     """
     Test that a file in the replica is updated to match the source.
     """
     file_name = 'file.txt'
     content = 'test update file'
-    with open(os.path.join(replica_folder, file_name), 'w', encoding='utf-8'):
+    with open(os.path.join(REPLICA_FOLDER, file_name), 'w', encoding='utf-8'):
         pass
 
-    with open(os.path.join(source_folder, file_name), 'w', encoding='utf-8') as f:
+    with open(os.path.join(SOURCE_FOLDER, file_name), 'w', encoding='utf-8') as f:
         f.write(content)
 
     folder_sync.synchronize()
 
-    with open(os.path.join(replica_folder, file_name), 'r', encoding='utf-8', ) as f:
+    with open(os.path.join(REPLICA_FOLDER, file_name), 'r', encoding='utf-8', ) as f:
         assert f.read() == content
